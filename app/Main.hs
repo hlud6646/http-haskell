3dde371481212aec540ab34f041382baf4f28eea
Hugo Ludemann <hugo.ludemann@gmail.com>
2024-08-08 02:06:24 +1000

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -13,16 +13,11 @@ main :: IO ()
 main = do
     hSetBuffering stdout LineBuffering
 
-    -- You can use print statements as follows for debugging, they'll be visible when running tests.
-    -- BC.putStrLn "Logs from your program will appear here"
-
-    -- Uncomment this block to pass first stage
     let host = "127.0.0.1"
         port = "4221"
     
     BC.putStrLn $ "Listening on " <> BC.pack host <> ":" <> BC.pack port
     
-    -- Get address information for the given host and port
     addrInfo <- getAddrInfo Nothing (Just host) (Just port)
     
     serverSocket <- socket (addrFamily $ head addrInfo) Stream defaultProtocol
@@ -30,16 +25,15 @@ main = do
     bind serverSocket $ addrAddress $ head addrInfo
     listen serverSocket 5
     
-    -- Accept connections and handle them forever
     forever $ do
         (clientSocket, clientAddr) <- accept serverSocket
         BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr) <> "."
 
         (req, _, _) <- parseMsg <$> recv clientSocket 1024
         sendAll clientSocket $ case parseReq req of
-            Nothing -> "?"
-            Just (_, "/", _) -> "HTTP/1.1 200 OK\r\n\r\n"
-            _ -> "HTTP/1.1 404 Not Found\r\n\r\n"
+            -- TODO: Is this an appropriate response?
+            Nothing -> "HTTP/1.1 500 Internal Server Error\r\n\r\n"
+            Just (method, target, _) -> route method (BC.unpack target)
 
         close clientSocket
 
@@ -57,4 +51,12 @@ parseReq req = case  BC.split ' ' req of
 data Method = GET | POST | PATCH | DELETE
     deriving (Eq, Show, Read)
 
--- Not given megaparsec, which I guess is too much for this.
+-- TODO: Use Method datatype not bytestring.
+route :: BC.ByteString -> String -> BC.ByteString
+route "GET" "/" = "HTTP/1.1 200 OK\r\n\r\n"
+route "GET" ('/' : 'e' : 'c' : 'h' : 'o' : '/' : text) = mkEchoResponse text
+route _ _  = "HTTP/1.1 404 Not Found\r\n\r\n"
+
+mkEchoResponse :: String -> BC.ByteString
+mkEchoResponse text = BC.pack $ "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " <> contentLength <> "\r\n\r\n" <> text
+    where contentLength = show . length $ text
